005b58af311fe9ce89a9641d6bc04c3f63810167
Zyy <No Email>
2024-12-24 17:41:22 +0800

    更新项目结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common'
+import { Module, RequestMethod } from '@nestjs/common'
+import { LoggerModule } from 'nestjs-pino'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRootAsync({
+      useFactory: async () => {
+        return {
+          pinoHttp: {
+            level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+            transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined
+          },
+          forRoutes: [AppController],
+          exclude: [{ method: RequestMethod.ALL, path: 'check' }]
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { bufferLogs: true })
+
+  app.useLogger(app.get(Logger))
+  app.useGlobalInterceptors(new LoggerErrorInterceptor())
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
