973df2db53936145bcb3bb86cf1f5efbf95f82e3
Zyy <No Email>
2024-12-25 15:38:09 +0800

    更新项目结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,30 @@
 import { Module, RequestMethod } from '@nestjs/common'
 import { LoggerModule } from 'nestjs-pino'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
   imports: [
     LoggerModule.forRootAsync({
-      useFactory: async () => {
-        return {
-          pinoHttp: {
-            level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
-            transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined
-          },
-          forRoutes: [AppController],
-          exclude: [{ method: RequestMethod.ALL, path: 'check' }]
-        }
-      }
-    })
+      useFactory: async () => ({
+        pinoHttp: {
+          level: 'debug',
+          transport:
+            process.env.NODE_ENV !== 'production'
+              ? {
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: true
+                  }
+                }
+              : undefined
+        },
+        forRoutes: [AppController],
+        exclude: [{ method: RequestMethod.ALL, path: 'check' }]
+      })
+    }),
+    ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService]
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true })
+  const app = await NestFactory.create(AppModule, { bufferLogs: true, logger: false })
 
   app.useLogger(app.get(Logger))
   app.useGlobalInterceptors(new LoggerErrorInterceptor())
 
   await app.listen(process.env.PORT ?? 3000)
+
+  const url = await app.getUrl()
+  console.log('\x1b[32m%s\x1b[0m', `\n\nApplication is running at: ${url}\n`)
 }
+
 bootstrap()
