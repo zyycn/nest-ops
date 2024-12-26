7f8d2b4d334154975c231d119664262c0fcf9248
Zyy <No Email>
2024-12-26 12:17:47 +0800

    更新项目结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,55 @@
-import { Module, RequestMethod } from '@nestjs/common'
+import { IncomingMessage } from 'http'
+import { randomUUID } from 'crypto'
+import { resolve } from 'node:path'
+import { Module } from '@nestjs/common'
+import { multistream } from 'pino'
 import { LoggerModule } from 'nestjs-pino'
 import { ConfigModule } from '@nestjs/config'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import dayjs from 'dayjs'
+import { AppService } from '@/app.service'
+import { AppController } from '@/app.controller'
 
 @Module({
   imports: [
     LoggerModule.forRootAsync({
       useFactory: async () => ({
-        pinoHttp: {
-          level: 'debug',
-          transport:
-            process.env.NODE_ENV !== 'production'
-              ? {
-                  target: 'pino-pretty',
-                  options: {
-                    singleLine: true
+        pinoHttp: [
+          {
+            quietReqLogger: true,
+            level: process.env.NODE_ENV !== 'production' ? 'info' : 'debug',
+            timestamp: () => `,"time":"${dayjs().format('YYYY-MM-DD HH:mm:ss')}"`,
+            genReqId: (req: IncomingMessage): string => {
+              const requestId = req.headers['x-request-id']
+              return typeof requestId === 'string' ? requestId : randomUUID()
+            },
+            transport:
+              process.env.NODE_ENV !== 'production'
+                ? {
+                    target: 'pino-pretty',
+                    options: {
+                      singleLine: true
+                    }
                   }
-                }
-              : undefined
-        },
-        forRoutes: [AppController],
-        exclude: [{ method: RequestMethod.ALL, path: 'check' }]
+                : undefined
+          },
+          multistream(
+            [
+              { level: 'debug', stream: process.stdout },
+              { level: 'error', stream: process.stderr },
+              { level: 'fatal', stream: process.stderr }
+            ],
+            { dedupe: true }
+          )
+        ]
       })
     }),
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`
+      load: [
+        async () => {
+          return await import(resolve(`config/${process.env.NODE_ENV}.ts`))
+        }
+      ]
     })
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import { Logger as NestLogger } from '@nestjs/common'
-import { AppModule } from './app.module'
+import { AppModule } from '@/app.module.js'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
