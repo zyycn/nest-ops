288d73531e3067e7ca15b930ef110c202b093066
Zyy <No Email>
2024-12-26 16:28:52 +0800

    更新项目结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { LoggerModule } from 'nestjs-pino'
 import { ConfigModule } from '@nestjs/config'
-import { loggerConfig } from '@/config/pino-logger'
+import { loggerConfig } from '@/config/logger.config'
 import { configuration } from '@/config/configuration'
 import { AppService } from '@/app.service'
 import { AppController } from '@/app.controller'
